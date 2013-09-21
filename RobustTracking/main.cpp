0129c0555bc9795f57d659ce218adbced32281fd
Alper Yilmaz <yilmaz.15@AlperMBP13.local>
2013-09-21 14:35:30 -0400

    Initial Commit

---
+++ RobustTracking/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RobustTracking
+//
+//  Created by Alper Yilmaz on 9/21/13.
+//  Copyright (c) 2013 Alper Yilmaz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
